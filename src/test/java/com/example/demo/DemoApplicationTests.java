f7167d058ae31ae1e0b000ae7a2f36a5d86de8db
VigneshGnanavel <167099865+VigneshGnanavel@users.noreply.github.com>
2024-07-24 16:58:43 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootApplication
+public class DemoApplication {
 
-	@Test
-	void contextLoads() {
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
 
+@RestController
+@RequestMapping("/api")
+class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Jenkins and AWS!";
+    }
 }
